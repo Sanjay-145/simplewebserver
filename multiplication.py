a2716d275618405bc3c7eff70de06bb4fe9059c8
Sanjay-145 <sanjayboven123@gmail.com>
2021-02-12 19:28:12 +0000

    webserver completed

--- multiplication.py
+++ multiplication.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer , BaseHTTPRequestHandler
 
 content =""" 
@@ -59,17 +41,3 @@ httpd = HTTPServer( server_address , Web )
 #To listen at the specified port
 print(" MY webserver is running !!!")
 httpd.serve_forever()
-```
-## OUTPUT:
-![output](./static/img/page.png)'
-
-## CODE VALIDATION REPORT:
-![output](./static/img/validation.png)
-
-## RESULT:
-Thus a website is designed for the simple webserver and is hosted in the 
-URL: http://sanjay.student.saveetha.in/multiplication.py. HTML code is validated.
-
-
-
-
